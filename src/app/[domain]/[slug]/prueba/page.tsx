c548060206c1aedb48f02c5d3f0314e6b83bbf7a
Royler Marichal Carrazana <cluzstudio@gmail.com>
2024-04-13 15:32:06 -0300

    Add new Test component to [domain]/[slug]/prueba/page.tsx

---
+++ src/app/[domain]/[slug]/prueba/page.tsx
@@ -0,0 +1,15 @@
+function Test() {
+    console.log("test");
+    
+  return (
+    <div>
+      <div className="ease left-0 right-0 top-0 z-30 flex h-16 bg-white transition-all duration-150 dark:bg-black dark:text-white">
+        <div className="mx-auto flex h-full max-w-screen-xl items-center justify-center space-x-5 px-10 sm:px-20">
+          test
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,20 @@ import { NextResponse } from "next/server";
 //import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/clerk", "/api/stripe", "/api/test","/api/cron"],
+  publicRoutes: [
+    "/",
+    "/test",
+    "/api/clerk",
+    "/api/stripe",
+    "/api/test",
+    "/api/cron",
+  ],
   afterAuth(auth, req) {
+    const url = req.nextUrl;
+    let hostname = req.headers
+      .get("host")!
+      .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+
     // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
@@ -23,8 +35,28 @@ export default authMiddleware({
     if (auth.userId && !auth.isPublicRoute) {
       return NextResponse.next();
     }
+
+    const searchParams = req.nextUrl.searchParams.toString();
+    // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+    const path = `${url.pathname}${
+      searchParams.length > 0 ? `?${searchParams}` : ""
+    }`;
+
+    // rewrite root application to `/home` folder
+    if (
+      hostname === "localhost:3000" ||
+      hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
+    ) {
+      return NextResponse.next();
+    }
+    // console.log("here");
+
+
+    console.log("here-"+ hostname +''+ path);
+    
+    // rewrite everything else to `/[domain]/[slug] dynamic route
+    return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
     // Allow users visiting public routes to access them
-    return NextResponse.next();
   },
 });
 
