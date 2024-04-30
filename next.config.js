e166456c16af1d639748eb0d4699845d4b10bc0b
Royler Marichal Carrazana <52290307+RoylerMarichal@users.noreply.github.com>
2024-04-30 10:07:40 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
