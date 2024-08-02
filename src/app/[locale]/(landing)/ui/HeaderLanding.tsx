c07ee7e3cb4bcf365deba8a1ac3d0e89932b8394
Royler Marichal Carrazana <cluzstudio@gmail.com>
2024-08-01 22:25:45 -0300

    refactor: Remove unnecessary code in HeaderLanding.tsx

--- src/app/[locale]/(landing)/ui/HeaderLanding.tsx
+++ src/app/[locale]/(landing)/ui/HeaderLanding.tsx
@@ -97,10 +97,7 @@ export const HeaderLanding = () => {
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">QvaHost Company</span>
-            </a>
-
+           
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-400"
